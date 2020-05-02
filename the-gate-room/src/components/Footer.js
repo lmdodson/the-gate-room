b0f8899243e1c6d4987ac722869f87dcb4f8b376
Leah <leahdodson9@gmail.com>
2020-05-02 00:41:05 -0500

    streamlined home page and cleaned up elements

--- the-gate-room/src/components/Footer.js
+++ the-gate-room/src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Door from "../../src/assets/Door.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
-    <img src={Door} alt="ornate door" width="30" height="50"></img>
-    <p>
-      {/* Sample project provided by <a href="https://auth0.com">Auth0</a> */}
-    </p>
+    <FontAwesomeIcon class="far fa-copyright">2020</FontAwesomeIcon>
+  <p className="mb-0">Think you've seen it all? Think again. Outside those doors, we might see anything. We could find new worlds, terrifying monsters, impossible things. And if you come with me... nothing will ever be the same again!</p>
+  <p className="quote-footer">--10th Doctor in <cite title="Source Title">Dr. Who</cite></p>
   </footer>
 );
 
