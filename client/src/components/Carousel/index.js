524e2c3026299fbc36acfe4d5efde3dd23b5078d
Leah <leahdodson9@gmail.com>
2020-05-11 01:58:02 -0500

    added Link

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useAuth0 } from "../../react-auth0-spa";
+import { Link } from "react-router-dom"
 
 import { Carousel } from 'react-responsive-carousel';
 
