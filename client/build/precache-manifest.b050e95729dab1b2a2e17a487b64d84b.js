fcd8ca532926b83b12e6a9a72a07c8bef4809569
Leah <leahdodson9@gmail.com>
2020-05-10 22:44:55 -0500

    deploying

---
+++ client/build/precache-manifest.b050e95729dab1b2a2e17a487b64d84b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30afc02d1d8ac1ec37e63f3ae205510d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "780746b88a5c162b674f",
+    "url": "/static/css/2.818df7c1.chunk.css"
+  },
+  {
+    "revision": "e753b53292da6df16dfc",
+    "url": "/static/css/main.6a4c5f07.chunk.css"
+  },
+  {
+    "revision": "780746b88a5c162b674f",
+    "url": "/static/js/2.3d3ad9dc.chunk.js"
+  },
+  {
+    "revision": "64f7e58d0b20d4d7a9a6188b47824605",
+    "url": "/static/js/2.3d3ad9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e753b53292da6df16dfc",
+    "url": "/static/js/main.37b9cfea.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "ff94b29d40eb3fba7d90875d9fc06343",
+    "url": "/static/media/Door.ff94b29d.jpg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  }
+]);
