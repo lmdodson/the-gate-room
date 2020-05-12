1b19bde0929a8ff4cef0ece21d063f0de06a9645
Leah <leahdodson9@gmail.com>
2020-05-12 14:54:41 -0500

    adjusted formatting in episodes and stores index files

--- client/src/components/Episodes/index.js
+++ client/src/components/Episodes/index.js
@@ -8,24 +8,29 @@ import "./style.css"
  
 const Episodes = () => {
   // define login variables
-        return (
-          <div>
-            <Carousel className="carousel-container d-lg-flex">
+    return (
+        <div className="d-lg-flex episode-container">
+            <div className="descriptions">
+              <h2 className="my-5 text-center">Among The Stars</h2>
+                <h4 className="my-5 text-center">Among The Stars journeys into the mind of a human trying to recover key information from hidden memories. Can you navigate through the mysteries to unravel the truth? Begin with Episode 1: Arrival.</h4>
+            </div>
+            <Carousel className="carousel-container">
                 <div>
                     <img src="https://cdn.pixabay.com/photo/2017/02/08/12/46/moon-2048727_1280.jpg" alt="swirling stars"/>
                     <Link to="/arrival" className="legend">Arrival</Link>
                 </div>
                 <div>
-                    <img src="https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_1280.jpg" alt="chalkboard coming soon" />
-                    <p className="legend">New Stories Coming Soon!</p>
+                    <img src="https://cdn.pixabay.com/photo/2019/02/21/05/02/lock-4010669_1280.jpg" alt="lock with key" />
+                    <p className="legend">Complete Episode 1: Arrival to unlock this Episode</p>
                 </div>
                 <div>
-                    <img src="https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_1280.jpg" alt="chalkboard coming soon" />
-                    <p className="legend">New Stories Coming Soon!</p>
+                    <img src="https://cdn.pixabay.com/photo/2019/02/21/05/02/lock-4010669_1280.jpg" alt="lock with key" />
+                    <p className="legend">Complete Episode 1: Arrival to unlock this Episode
+                    </p>
                 </div>
             </Carousel>
-            </div>
-        )
-    }
+        </div>
+    )
+}
 
 export default Episodes;
--- client/src/components/Stories/index.js
+++ client/src/components/Stories/index.js
@@ -11,28 +11,28 @@ const Exps = () => {
   // define login variables
   const {user} = useAuth0();
 
-        return (
-          <div className="d-lg-flex story-container">
-            <div className="instructions">
-              <h2 className="my-5 text-center">Hello {user.nickname}</h2>
-                <h4 className="my-5 text-center">Check out the current stories. Click on a link to get started!</h4>
-                </div>
-            <Carousel className="carousel-container d-lg-flex">
-                <div>
-                    <img src="https://cdn.pixabay.com/photo/2017/02/08/12/46/moon-2048727_1280.jpg" alt="swirling stars"/>
-                    <Link to="/ats" className="legend">Among the Stars</Link>
-                </div>
-                <div>
-                    <img src="https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_1280.jpg" alt="chalkboard coming soon" />
-                    <p className="legend">New Stories Coming Soon!</p>
-                </div>
-                <div>
-                    <img src="https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_1280.jpg" alt="chalkboard coming soon" />
-                    <p className="legend">New Stories Coming Soon!</p>
-                </div>
-            </Carousel>
-          </div>
-        )
-    }
+  return (
+    <div className="d-lg-flex story-container">
+      <div className="instructions">
+        <h2 className="my-5 text-center">Hello {user.nickname}</h2>
+          <h4 className="my-5 text-center">Check out the current stories. Click on a link to get started!</h4>
+      </div>
+      <Carousel className="carousel-container d-lg-flex">
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2017/02/08/12/46/moon-2048727_1280.jpg" alt="swirling stars"/>
+          <Link to="/ats" className="legend">Among the Stars</Link>
+        </div>
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_1280.jpg" alt="chalkboard coming soon" />
+          <p className="legend">New Stories Coming Soon!</p>
+        </div>
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_1280.jpg" alt="chalkboard coming soon" />
+          <p className="legend">New Stories Coming Soon!</p>
+        </div>
+      </Carousel>
+    </div>
+  )
+}
 
 export default Exps;
